c1a90bdbb3cbe20bbad9512b8dbc8d5cbc1ccbf6
Adetunji Samuel <samadet0001@gmail.com>
2024-06-12 04:57:23 +0200

    update

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -1,16 +1,26 @@
 import AllLayout from "@/components/layout/AllLayout";
+import FirstLayout from "@/components/layout/firstLayout";
+import SecondLayout from "@/components/layout/secondLayout";
 import { createClient } from "@/supabase/server";
 import { redirect } from "next/navigation";
+import { HiBars3CenterLeft } from "react-icons/hi2";
 
 export default async function Layout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const supabase = await createClient();
-  const { data, error } = await supabase.auth.getUser();
-  if (error || !data?.user) {
-    redirect("/login");
-  }
-  return <AllLayout children={children} />;
+  // const supabase = await createClient();
+  // const { data, error } = await supabase.auth.getUser();
+  // if (error || !data?.user) {
+  //   redirect("/login");
+  // }
+  return (
+    <div className="grid grid-cols-12 gap-6 h-screen">
+      <FirstLayout />
+      <div className="col-span-12 sm:col-span-8 md:col-span-9 h-screen sm:overflow-y-scroll bg-white p-4 ">
+        <div className=" p-4">{children}</div>
+      </div>{" "}
+    </div>
+  );
 }
--- components/layout/firstLayout.tsx
+++ components/layout/firstLayout.tsx
@@ -3,82 +3,70 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useState } from "react";
+import { BsChat } from "react-icons/bs";
 import { HiXMark } from "react-icons/hi2";
+import { SlSpeech } from "react-icons/sl";
 
-export default function FirstLayout({ open, setOpen }: any) {
+export default function FirstLayout() {
   const pathname = usePathname();
 
   return (
-    <div
-      className={
-        !open
-          ? "hidden  sm:grid min-h-screen  sm:static sm:col-span-4 md:col-span-3 bg-gray-200 p-4"
-          : " fixed min-h-screen sm:grid sm:static sm:col-span-4 md:col-span-3 bg-gray-200 p-4"
-      }
-    >
-      <div>
-        <HiXMark
-          onClick={() => setOpen(false)}
-          className="size-8 cursor-pointer sm:hidden"
-        />
-      </div>
-      <h2 className="font-bold text-xl md:text-2xl text-center mb-16 pt-10 sm:pt-0">
+    <div className=" sm:min-h-screen sm:grid sm:static sm:col-span-4 md:col-span-3 bg-none sm:bg-gray-200 sm:p-4">
+      <h2 className="hidden sm:flex font-bold text-xl md:text-2xl text-center mb-16 pt-10 sm:pt-0">
         Xocial
       </h2>
-      <ul className="w-full lg:w-[80%] lg:mx-auto">
+      <ul className="w-full px-3 py-2 justify-between items-center  flex right-0 left-0 bg-slate-200 sm:bg-none fixed bottom-0 sm:flex-none lg:w-[80%] lg:mx-auto">
         <li
           className={
             pathname === "/app"
-              ? "mb-8 w-full bg-black text-center text-white font-medium text-[16px] px-2 py-2 rounded hover:bg-black hover:text-white"
-              : "mb-8 w-full bg-white text-black text-center font-medium text-[16px] px-2 py-2 rounded hover:bg-black hover:text-white"
+              ? "sm:mb-8 w-fit sm:w-full bg-black text-center text-white font-medium text-[16px] px-2 py-2 rounded hover:bg-black hover:text-white"
+              : "sm:mb-8 w-fit sm:w-full bg-white text-black text-center font-medium text-[16px] px-2 py-2 rounded hover:bg-black hover:text-white"
           }
         >
-          <Link href="/app">Tweet</Link>
+          <Link href="/app">
+            <SlSpeech className="size-8 text-center hover:fill-white" />
+            <span className="sm:flex hidden">Tweet</span>
+          </Link>
         </li>
 
         <li
           className={
             pathname === "/app/notifications"
-              ? "mb-8 w-full bg-black text-center text-white font-medium text-[16px] px-2 py-2 rounded hover:bg-black hover:text-white"
-              : "mb-8 w-full bg-white text-black text-center font-medium text-[16px] px-2 py-2 rounded hover:bg-black hover:text-white"
+              ? "sm:mb-8 w-fit sm:w-full bg-black text-center text-white font-medium text-[16px] px-2 py-2 rounded hover:bg-black hover:text-white"
+              : "sm:mb-8 w-fit sm:w-full bg-white text-black text-center font-medium text-[16px] px-2 py-2 rounded hover:bg-black hover:text-white"
           }
         >
-          <Link href="/app/notifications">Notifications</Link>
+          <Link href="/app/notifications">
+            <BsChat className="size-8 text-center hover:fill-white" />
+            <span className="sm:flex hidden">Notification</span>
+          </Link>
         </li>
 
         <li
           className={
             pathname === "/app/people"
-              ? "md:hidden mb-8 w-full bg-black text-center text-white font-medium text-[16px] px-2 py-2 rounded hover:bg-black hover:text-white"
-              : " md:hidden mb-8 w-full bg-white text-black text-center font-medium text-[16px] px-2 py-2 rounded hover:bg-black hover:text-white"
+              ? "md:hidden sm:mb-8 w-fit sm:w-full bg-black text-center text-white font-medium text-[16px] px-2 py-2 rounded hover:bg-black hover:text-white"
+              : " md:hidden sm:mb-8 w-fit sm:w-full bg-white text-black text-center font-medium text-[16px] px-2 py-2 rounded hover:bg-black hover:text-white"
           }
         >
           <Link href="/app/people">People</Link>
         </li>
-        <li
-          className={
-            pathname === "/app/likedpost"
-              ? "mb-8 w-full bg-black text-center text-white font-medium text-[16px] px-2 py-2 rounded hover:bg-black hover:text-white"
-              : "mb-8 w-full bg-white text-black text-center font-medium text-[16px] px-2 py-2 rounded hover:bg-black hover:text-white"
-          }
-        >
-          <Link href="/app/likedpost">Liked Posts</Link>
-        </li>
+
         <li
           className={
             pathname === "/app/profile"
-              ? "mb-8 w-full bg-black text-center text-white font-medium text-[16px] px-2 py-2 rounded hover:bg-black hover:text-white"
-              : "mb-8 w-full bg-white text-black text-center font-medium text-[16px] px-2 py-2 rounded hover:bg-black hover:text-white"
+              ? "sm:mb-8 w-fit sm:w-full bg-black text-center text-white font-medium text-[16px] px-2 py-2 rounded hover:bg-black hover:text-white"
+              : "sm:mb-8 w-fit sm:w-full bg-white text-black text-center font-medium text-[16px] px-2 py-2 rounded hover:bg-black hover:text-white"
           }
         >
           <Link href="/app/profile">Profile</Link>
         </li>
       </ul>
-      <div className="pt-10  md:w-[80%] md:mx-auto">
+      {/* <div className="hidden sm:flex pt-10  md:w-[80%] md:mx-auto">
         <button className="w-full bg-white text-slate-800 text-center font-medium text-[16px] px-2 py-2 rounded hover:bg-slate-600 hover:text-white">
           Logout
         </button>
-      </div>
+      </div> */}
     </div>
   );
 }
